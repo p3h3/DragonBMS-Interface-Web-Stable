3827f839611b1eca0e0ef1e9c021d24819b25c81
Peter <hi@p3h3.de>
2023-02-21 11:30:35 +0100

    fixed wrong serviceworker path

--- js/serviceworker/sw_init.js
+++ js/serviceworker/sw_init.js
@@ -1,1 +1,1 @@
-"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/DragonBMS-Interface-Web/serviceworker.js")});
+"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("serviceworker.js")});
